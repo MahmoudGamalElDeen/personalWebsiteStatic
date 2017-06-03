4bce666312aec2cbab3fd45b33e3399d3e369e42
FlatlanderWoman <michaelbassili539@gmail.com>
2017-06-03 10:54:24 -0700

    Ported over pportfolio site, and templated all fields.

---
+++ README.md
@@ -0,0 +1,20 @@
+# [Portfolio](http://bassi.li) - v3.0 
+#### The source code for my personal website & portfolio- which hosts my work as well as some relevant contact information.
+
+## Overview
+
+#### About the Website
+This is a clean and succinct place to showcase the things I build, design and publish. Older versions of this site will be cached to preserve their memories (and their code) while also allowing me to upgrade the website over time (up to version 2- version 3 and beyond are contained in branches). See the branches/projects to view the currently developed version.
+
+#### Versions
+- [Version 1](http://bassi.li/v1/)
+- [Version 2](http://bassi.li/v2/)
+- [Version 3](http://bassi.li/)
+
+#### Issue Contribution
+If you find any issues, let me know- submit an issue to the repository.
+
+### Installation
+```
+> git clone https://github.com/FlatlanderWoman/portfolio.git
+```
---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+// Simple Sites :: scripts.js
+
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-77100076-2', 'auto');
+ga('send', 'pageview');
+
+// Modal
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
